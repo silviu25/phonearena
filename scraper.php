7c8ffef95cef17f447e672803c66d8e185dc1994
silviu25 <silviu_25@yahoo.com>
2016-06-28 12:47:54 +0300

    Fork of code from ScraperWiki at https://classic.scraperwiki.com/scrapers/phonearena/

---
+++ scraper.php
@@ -0,0 +1,19 @@
+<?php
+     $url = 'http://www.phonearena.com/phones/Samsung-Infuse-4G_id5119/fullspecs';
+     $html = scraperwiki::scrape($url);  
+     require 'scraperwiki/simple_html_dom.php';
+              $dom = new simple_html_dom();
+              $dom->load($html);
+     //$dom->remove('span[class=s_tooltip_content]');
+                 foreach($dom->find('div[class=s_specs_box] li[class=s_lv_1] ul[class=s_lv_2] li') as $data)
+                 {
+                    
+
+                    if($data->parent()->parent()->getElementByTagName('strong[class=s_lv_2] span[class=s_tooltip_anchor]'))
+                        print $data->parent()->parent()->getElementByTagName('strong[class=s_lv_2] span[class=s_tooltip_anchor]')->plaintext." : ";
+                    else
+                        print $data->parent()->parent()->getElementByTagName('strong[class=s_lv_2]')->plaintext." : ";
+
+                     print $data->plaintext."\n";
+                 }          
+ ?>
